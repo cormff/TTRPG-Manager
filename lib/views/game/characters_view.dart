64025cf6ce831181dde3d2db8b20a4a3aff51851
Your Name <you@example.com>
2026-05-06 15:15:17 +0300

    Merge tamamlandı. Iteration 2 versiyonu tekrar push.

--- lib/views/game/characters_view.dart
+++ lib/views/game/characters_view.dart
@@ -14,6 +14,7 @@ class CharactersView extends StatefulWidget {
 }
 
 
+
 class _CharactersViewState extends State<CharactersView> {
   static const List<String> availableAvatars = [
     'assets/images/avatars/human_fighter.png',
